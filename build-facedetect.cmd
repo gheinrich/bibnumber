38f30ae53be613cfc64cf4400aa11f25b5acd8d1
Greg Heinrich <gregory.heinrich@gmail.com>
2015-01-18 20:52:54 +0000

    Initial revision

---
+++ build-facedetect.cmd
@@ -0,0 +1,1 @@
+g++ -o facedetect facedetect.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_objdetect
---
+++ exec-facedetect.cmd
@@ -0,0 +1,1 @@
+./facedetect --cascade=/home/greg/ws/opencv/data/haarcascades/haarcascade_frontalface_alt.xml ~/host/prom.jpg
